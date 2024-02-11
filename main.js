8a086fc658b077c4c0f21e6f344f6527241b270e
songajeung <thddkwjd@naver.com>
2024-02-11 17:10:03 +0900

    secound commit

--- main.js
+++ main.js
@@ -12,8 +12,17 @@ let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history = [];
 
 playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", function () {
+  userInput.value = "";
+}); //이 함수가 다른곳에 안쓰거나 간단한 로직일때 선언과 함수 같이 쓴다
 
 function pickRandomNum() {
   computerNum = Math.floor(Math.random() * 100) + 1;
@@ -24,6 +33,23 @@ function pickRandomNum() {
 
 function play() {
   let userValue = userInput.value;
+
+  if (userValue < 1 || userValue > 100) {
+    resultArea.textContent = "1과 100사이 숫자를 입력해 주세요";
+    return;
+  }
+
+  if (history.includes(userValue)) {
+    resultArea,
+      (textContent = "이미 입력한 숫자입니다 다른 숫자를 입력해 주세요");
+    return;
+  }
+
+  chances--;
+  chanceArea.textContent = `남은찬스:${chances}번`;
+
+  //${} -> 동적인 값 넣을수있음
+
   if (userValue < computerNum) {
     resultArea.textContent = "Up!";
     // console.log("Up!!");
@@ -33,7 +59,30 @@ function play() {
   } else {
     resultArea.textContent = "정답입니다.";
     // console.log("정답입니다");
+    gameOver = true;
   }
+
+  history.push(userValue);
+  //   console.log(history);
+
+  if (chances < 1) {
+    gameOver = true;
+  }
+
+  if (gameOver == true) {
+    playButton.disabled = true;
+  }
+}
+
+function reset() {
+  //user input 창이 깨끗하게 정리되고
+  userInput.value = "";
+  //새로운 번호가 생성, 부여된다
+  pickRandomNum();
+
+  resultArea.textContent = "결과값이 여기 나옵니다";
+
+  chanceArea.textContent = "남은 기회는? 5번";
 }
 
 pickRandomNum();
